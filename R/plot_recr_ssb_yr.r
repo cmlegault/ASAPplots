20d34ede3e63770dfbfb79a9b6fea904ad0e69ed
cmlegault <chris.legault@noaa.gov>
2017-11-28 15:05:33 -0500

    Add R/SSB time series plot

--- R/plot_recr_ssb_yr.r
+++ R/plot_recr_ssb_yr.r
@@ -1,6 +1,6 @@
 #' Scatterplot of SSB, R with 2-digit year  
 #' 
-#' Standard stock-recruitment plot with years labeled.
+#' Standard stock-recruitment plot with years labeled. Also creates R/SSB time series plot.
 #' @param asap name of the variable that read in the asap.rdat file
 #' @param save.plots save individual plots
 #' @param od output directory for plots and csv files 
@@ -15,11 +15,11 @@ PlotRecrSSByr <- function(asap,save.plots,od,plotf){
   recr <- asap$N.age[,1]
   years <- seq(asap$parms$styr, asap$parms$endyr)
   nyears <- length(years)
-  SR <- matrix(NA, (nyears-1), 3)
+  SR <- matrix(NA, (nyears-1), 4)
   SR[,1] <- years[1:(nyears-1)]
   SR[,2] <- ssb[1:(nyears-1)]
   SR[,3] <- recr[2:nyears]
-  
+  SR[,4] <- SR[,3] / SR[,2]  # R per SSB
   
   yr.text=substr(SR[,1],3,4)
   npts<-length(years)-1
@@ -33,5 +33,11 @@ PlotRecrSSByr <- function(asap,save.plots,od,plotf){
   
   if (save.plots==T) savePlot( paste(od, "S_R.Yr.",plotf, sep=""), type=plotf)
   
+  plot(SR[,1],SR[,4], type='o', col='black', pch=16, 
+       xlab="Year", ylab="R/SSB")
+  abline(h=mean(SR[,4]), lty=2)
+  if(save.plots==TRUE) savePlot(paste0(od, "RpSSB_time_series.",plotf), type=plotf)
+  
   return()
+  
 }  #end function
