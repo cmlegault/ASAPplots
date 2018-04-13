5c6fd3cd032bfc23f6445736ed855412d5304d72
cmlegault <chris.legault@noaa.gov>
2018-04-13 13:14:56 -0400

    fix CI labels in ASAP_summary_xxx.csv

--- R/summarize_ASAP.r
+++ R/summarize_ASAP.r
@@ -19,9 +19,9 @@ SummarizeASAP <- function(asap,a1,od){
   recr_lo <-  asap$N.age[,1] - 1.96*recr.std
   Frep_up <-  asap$F.report + 1.96*Frep.std
   Frep_lo <-  asap$F.report - 1.96*Frep.std
-  mwg.out <- cbind("Year"=years, "SSB"=asap$SSB, "SSB_5"=ssb_lo, "SSB_95"=ssb_up,
-                   "Recr"=asap$N.age[,1], "Recr_5"=recr_lo, "Recr_95"=recr_up,
-                   "Freport"=asap$F.report, "Freport_5"=Frep_lo, "Freport_95"=Frep_up  )
+  mwg.out <- cbind("Year"=years, "SSB"=asap$SSB, "SSB_95_lo"=ssb_lo, "SSB_95_hi"=ssb_up,
+                   "Recr"=asap$N.age[,1], "Recr_95_lo"=recr_lo, "Recr_95_hi"=recr_up,
+                   "Freport"=asap$F.report, "Freport_95_lo"=Frep_lo, "Freport_95_hi"=Frep_up  )
   
   write.csv(mwg.out, paste0(od,"ASAP_summary_",asap.name,".csv"), row.names=F)
   return()
