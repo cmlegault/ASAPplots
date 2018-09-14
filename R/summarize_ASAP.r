1a90bc3a9ad58151e4b18a5c7f1444f62e318856
cmlegault <chris.legault@noaa.gov>
2018-09-14 09:29:37 -0400

    check for std file

--- R/summarize_ASAP.r
+++ R/summarize_ASAP.r
@@ -8,21 +8,23 @@
 
 SummarizeASAP <- function(asap,a1,od){
   
-  asap.name <- a1$asap.name
-  years <- seq(asap$parms$styr, asap$parms$endyr)
-  ssb.std <- a1$asap.std[which(a1$asap.std$name=="SSB") ,4]
-  recr.std <- a1$asap.std[which(a1$asap.std$name=="recruits") ,4]
-  Frep.std <- a1$asap.std[which(a1$asap.std$name=="Freport") ,4]
-  ssb_up <-  asap$SSB + 1.96*ssb.std
-  ssb_lo <-  asap$SSB - 1.96*ssb.std
-  recr_up <-  asap$N.age[,1] + 1.96*recr.std
-  recr_lo <-  asap$N.age[,1] - 1.96*recr.std
-  Frep_up <-  asap$F.report + 1.96*Frep.std
-  Frep_lo <-  asap$F.report - 1.96*Frep.std
-  mwg.out <- cbind("Year"=years, "SSB"=asap$SSB, "SSB_95_lo"=ssb_lo, "SSB_95_hi"=ssb_up,
-                   "Recr"=asap$N.age[,1], "Recr_95_lo"=recr_lo, "Recr_95_hi"=recr_up,
-                   "Freport"=asap$F.report, "Freport_95_lo"=Frep_lo, "Freport_95_hi"=Frep_up  )
-  
-  write.csv(mwg.out, paste0(od,"ASAP_summary_",asap.name,".csv"), row.names=F)
+  if(!is.na(a1$asap.std)){
+    asap.name <- a1$asap.name
+    years <- seq(asap$parms$styr, asap$parms$endyr)
+    ssb.std <- a1$asap.std[which(a1$asap.std$name=="SSB") ,4]
+    recr.std <- a1$asap.std[which(a1$asap.std$name=="recruits") ,4]
+    Frep.std <- a1$asap.std[which(a1$asap.std$name=="Freport") ,4]
+    ssb_up <-  asap$SSB + 1.96*ssb.std
+    ssb_lo <-  asap$SSB - 1.96*ssb.std
+    recr_up <-  asap$N.age[,1] + 1.96*recr.std
+    recr_lo <-  asap$N.age[,1] - 1.96*recr.std
+    Frep_up <-  asap$F.report + 1.96*Frep.std
+    Frep_lo <-  asap$F.report - 1.96*Frep.std
+    mwg.out <- cbind("Year"=years, "SSB"=asap$SSB, "SSB_95_lo"=ssb_lo, "SSB_95_hi"=ssb_up,
+                     "Recr"=asap$N.age[,1], "Recr_95_lo"=recr_lo, "Recr_95_hi"=recr_up,
+                     "Freport"=asap$F.report, "Freport_95_lo"=Frep_lo, "Freport_95_hi"=Frep_up  )
+    
+    write.csv(mwg.out, paste0(od,"ASAP_summary_",asap.name,".csv"), row.names=F)
+  }
   return()
 }
