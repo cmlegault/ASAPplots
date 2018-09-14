abcff366e2c05f904337c573eb131309c481ecd3
cmlegault <chris.legault@noaa.gov>
2018-09-14 09:54:34 -0400

    check for std file

--- R/plot_CV.r
+++ R/plot_CV.r
@@ -9,24 +9,26 @@
 #' @export
 
 PlotCV <- function(asap,a1,save.plots,od,plotf){
-  par(mfrow=c(1,1), mar=c(4,4,2,2)  )
-  
-  years = seq(asap$parms$styr, asap$parms$endyr)
-  asap.std <- a1$asap.std
-  F.rep = asap.std[asap.std$name=="Freport",3]  
-  cv.F.rep = asap.std[asap.std$name=="Freport",4]/F.rep
-  ssb.vec = asap.std[ asap.std$name=="SSB",3]
-  cv.ssb.vec = asap.std[ asap.std$name=="SSB",4]/ssb.vec
-  rec.vec = asap.std[ asap.std$name=="recruits", 3]
-  cv.rec.vec = asap.std[asap.std$name=="recruits", 4]/rec.vec
-
-  plot(years,   cv.rec.vec, type='l', lwd=2, col='black', xlab="Year", ylab="CV",
-       ylim=c(0, 1.1*max(cv.rec.vec, cv.ssb.vec, cv.F.rep))  )
-  lines(years, cv.ssb.vec, lwd=2, col="blue")
-  lines(years, cv.F.rep, lwd=2, col="green3")
-  legend('top', legend=c("Recr.", "SSB", "Freport"), col=c("black", "blue", "green3"),
-         lty=rep(1,3), lwd=rep(2,3), horiz=T)
-  
-  if (save.plots==T) savePlot(paste(od, 'CV.estimates.', plotf, sep=''), type=plotf)
+  if (!is.na(a1$asap.std)){
+    par(mfrow=c(1,1), mar=c(4,4,2,2)  )
+    
+    years = seq(asap$parms$styr, asap$parms$endyr)
+    asap.std <- a1$asap.std
+    F.rep = asap.std[asap.std$name=="Freport",3]  
+    cv.F.rep = asap.std[asap.std$name=="Freport",4]/F.rep
+    ssb.vec = asap.std[ asap.std$name=="SSB",3]
+    cv.ssb.vec = asap.std[ asap.std$name=="SSB",4]/ssb.vec
+    rec.vec = asap.std[ asap.std$name=="recruits", 3]
+    cv.rec.vec = asap.std[asap.std$name=="recruits", 4]/rec.vec
+    
+    plot(years,   cv.rec.vec, type='l', lwd=2, col='black', xlab="Year", ylab="CV",
+         ylim=c(0, 1.1*max(cv.rec.vec, cv.ssb.vec, cv.F.rep))  )
+    lines(years, cv.ssb.vec, lwd=2, col="blue")
+    lines(years, cv.F.rep, lwd=2, col="green3")
+    legend('top', legend=c("Recr.", "SSB", "Freport"), col=c("black", "blue", "green3"),
+           lty=rep(1,3), lwd=rep(2,3), horiz=T)
+    
+    if (save.plots==T) savePlot(paste(od, 'CV.estimates.', plotf, sep=''), type=plotf)
+  }
   return()
 }  # end function
